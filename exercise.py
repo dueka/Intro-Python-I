aaff92c253d58a14846c4072bc6d3438bd2cde8d
dueka <ekanemd@live.com>
2020-04-06 15:37:19 +0100

    Average speed

--- exercise.py
+++ exercise.py
@@ -4,9 +4,23 @@ Created on Mon Apr  6 11:59:50 2020
 
 @author: USER
 """
-
+# Exercise 1
 def SecondsInMinutes():
     minutes = int
     minutes = 21.15
     return (minutes * 60)
 print(SecondsInMinutes())
+
+def MilesInKilometres():
+    kilometres = int
+    kilometres = 5
+    return (kilometres * 0.621371)
+print(MilesInKilometres())
+
+def AveragePace():
+    kilometres = int
+    time = float
+    kilometres = 5
+    time = 21.15
+    return (kilometres / time)
+print(AveragePace())
